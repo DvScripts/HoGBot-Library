2087ea66c06873a44570d92488a8c6270b573379
DvScripts <57599756+DvScripts@users.noreply.github.com>
2024-03-09 12:05:07 +0100

    Update hogbot.lua

--- lib/hogbot.lua
+++ lib/hogbot.lua
@@ -2644,3 +2644,20 @@ function dropflask()
         dropitems(i, flaskCount)
     end
 end
+
+--- use lever on x y z position and ID or if no ID then default lever id
+--- @author  dworak
+--- @return  nil
+function uselever(x,y,z,id)
+    local levers = {2772, 2773}
+	if id then
+		levers = {id}
+	end
+	local curPosz = posz()
+	local itemposition = Position:new(x, y, z)
+	while curPosz == posz() do
+	    local topUseId = topuseitem(itemposition).id
+        useobject(itemposition, topUseId, 0, 0xFF)
+        wait(400,600)
+    end
+end
